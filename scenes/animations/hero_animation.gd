3a024f9b35c68635bfe7db5988d593ed96a2d952
Pigzinzspace <79694583+Pigzinzspace@users.noreply.github.com>
2021-12-15 16:43:47 +0400

    я даже не помню

--- scenes/animations/hero_animation.gd
+++ scenes/animations/hero_animation.gd
@@ -1,9 +1,30 @@
 extends Node2D
 
+#этот нод не учитывает хп или урон только анимацию, 
+# он умееет запускать по вызову set_new_action(action) анимацию
+# и сообщать что она закончилась сигналом action_ended(action) 
+# а так же прирывать анимацию   force_stop_action() 
+# если эту анимацию можно прервать моб зациклится в IDLE акции, и будет слать сигналы каждый цикл 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var animation_tree = $body/AnimationTree 
+var current_tree_action = {
+# value : start time
+}
+
+signal action_ended(action)	
+const animation_stopble = {
+	MyGlobal.mob_tree_action.MEDIUM_ATTACK : 1000,
+	MyGlobal.mob_tree_action.STRONG_ATTACK : 1500
+}
+const animation_to_mob_tree_action = {
+	"idle": MyGlobal.mob_tree_action.IDLE,
+	"panic": MyGlobal.mob_tree_action.PANIC,
+	"basic_attack" : MyGlobal.mob_tree_action.BASIC_ATTACK,
+	"medium_attack" : MyGlobal.mob_tree_action.MEDIUM_ATTACK,
+	"strong_attack" : MyGlobal.mob_tree_action.STRONG_ATTACK,
+	"death" : MyGlobal.mob_tree_action.DEATH
+}
+
 
 
 # Called when the node enters the scene tree for the first time.
